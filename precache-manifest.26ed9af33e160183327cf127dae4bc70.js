953caf588fc7442749ddc8d17e4e514a1db6c618
Shree Nair <gopu9363@gmail.com>
2022-06-28 14:52:09 +0530

    Updates

---
+++ precache-manifest.26ed9af33e160183327cf127dae4bc70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b4eba786013d463cfeeada713e11ed",
+    "url": "/react_repo/index.html"
+  },
+  {
+    "revision": "a98fa75a3f8ed929e90e",
+    "url": "/react_repo/static/css/main.e10d6157.chunk.css"
+  },
+  {
+    "revision": "33c23c39e5fc5a0a2a54",
+    "url": "/react_repo/static/js/2.eebb8e02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_repo/static/js/2.eebb8e02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a98fa75a3f8ed929e90e",
+    "url": "/react_repo/static/js/main.3703a794.chunk.js"
+  },
+  {
+    "revision": "df3f8a5e47067e927312",
+    "url": "/react_repo/static/js/runtime-main.a32678c9.js"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/react_repo/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/react_repo/static/media/spinner.235b9b3e.gif"
+  }
+]);
